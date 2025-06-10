1f0dd7085b1661d30106652150dc707bff9c1cd5
karim-99-99 <kareem.hossam99@yahoo.com>
2025-06-10 17:23:15 +0300

    k

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,8 +4,8 @@ function Header() {
   return (
     <div className='flex flex-col items-center p-4 gap-3'>
       <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="logo"  style={{ width: '50px', height: '50px'  }} />
-    <h1>Menna Yasser</h1>
-    <p>Hello my name is Menna Yasser but they call me hankaresha el resha</p>
+    <h1>karim khamis</h1>
+    <p>Hello my name is karim khamis this is my project it is callender i can save any appointment for any time and have option to delete or edit it</p>
     </div>
   )
 }
